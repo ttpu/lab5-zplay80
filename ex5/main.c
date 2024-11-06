a9e30bbc8d1f77924b892e3542c409b496febcbb
zplay80 <husniddin.mahkamov880@gmail.com>
2024-11-06 06:05:37 +0000

    finished ex5

--- ex5/main.c
+++ ex5/main.c
@@ -1,7 +1,19 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    char lowercase, uppercase;
+    int ascii_lower, ascii_upper;
+
+    printf("Lowercase\tASCII\tUppercase\tASCII\n");
+    
+    for (int i = 0; i < 26; i++) {
+        lowercase = 'a' + i;  
+        ascii_lower = lowercase;  
+        uppercase = 'A' + i;  
+        ascii_upper = uppercase;  
+
+        printf("%c\t\t%d\t%c\t\t%d\n", lowercase, ascii_lower, uppercase, ascii_upper);
+    }
+
+    return 0;
 }
