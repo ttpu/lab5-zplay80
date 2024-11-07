63c972f7fdd06fd538a6a55cb5d2aad616a6c4d6
zplay80 <husniddin.mahkamov880@gmail.com>
2024-11-07 16:45:34 +0000

    finished ex8

--- ex8/main.c
+++ ex8/main.c
@@ -1,7 +1,41 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    int n;
+
+    printf("Enter the number of values: ");
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("The number of values must be at least 2.\n");
+        return 1;
+    }
+
+    float numbers[n];
+
+    printf("Enter %d real values: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%f", &numbers[i]);
+    }
+
+   float largest = numbers[0], second_largest = numbers[1];
+
+    if (second_largest > largest) {
+        float temp = largest;
+        largest = second_largest;
+        second_largest = temp;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (numbers[i] > largest) {
+            second_largest = largest;
+            largest = numbers[i];
+        } else if (numbers[i] > second_largest) {
+            second_largest = numbers[i];
+        }
+    }
+
+    printf("The two largest values are: %.2f and %.2f\n", largest, second_largest);
+
+    return 0;
 }
