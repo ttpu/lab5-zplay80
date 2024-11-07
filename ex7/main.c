c0f980ffcdb35e499ec502e9d0d98b92bbef9329
zplay80 <husniddin.mahkamov880@gmail.com>
2024-11-07 16:38:57 +0000

    finished ex7

--- ex7/main.c
+++ ex7/main.c
@@ -1,7 +1,42 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    int n;
+
+    printf("Enter the number of integers (n): ");
+    scanf("%d", &n);
+
+   if (n <= 0) {
+        printf("Please enter a positive integer for n.\n");
+        return 1;
+    }
+
+    int numbers[n];
+
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    int is_ascending = 1;
+    int is_descending = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (numbers[i] <= numbers[i - 1]) {
+            is_ascending = 0;
+        }
+        if (numbers[i] >= numbers[i - 1]) {
+            is_descending = 0;
+        }
+    }
+
+    if (is_ascending) {
+        printf("ascending sequence\n");
+    } else if (is_descending) {
+        printf("descending sequence\n");
+    } else {
+        printf("neither ascending nor descending sequence\n");
+    }
+
+    return 0;
 }
